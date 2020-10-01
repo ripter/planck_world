752ee7a5402e6da203b9fed98c1c1d22d5854177
ripter <ripter001@gmail.com>
2020-10-01 14:19:56 -0700

    Adding Orbit controls

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import { createQuantumField } from './createQuantumField';
 
@@ -15,11 +16,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-// var geometry = new BoxGeometry();
-// var material = new MeshBasicMaterial( { color: 0x00ff00 } );
-// var cube = new Mesh( geometry, material );
-// scene.add( cube );
-
+// Setup the user controls.
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const field = createQuantumField(3, 3, 3);
 field.position.x = -1.5;
@@ -34,8 +32,6 @@ window.scene = scene;
 const animate = function () {
   requestAnimationFrame( animate );
 
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
 
   renderer.render( scene, camera );
 };
